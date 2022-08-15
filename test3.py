7a58ef778d343852dab83e4a1553f68107c408b0
LizandroV <lizandrovivanco16@gmail.com>
2022-08-15 10:31:40 -0500

    se agrego el nuevo icono en la app

--- test3.py
+++ test3.py
@@ -1,23 +1,43 @@
-from win32api import *
-  
-def get_version_number(file_path):
-  
-    File_information = GetFileVersionInfo(file_path, "\\")
-  
-    ms_file_version = File_information['FileVersionMS']
-    ls_file_version = File_information['FileVersionLS']
-  
-    return [str(HIWORD(ms_file_version))]
-  
-file_path = r'C:\Program Files\Fortinet\FortiClient\FortiClient.exe'
-  
-version = ".".join(get_version_number(file_path))
-  
-print(version)
-version2 = int(version)
+import os
+import base64
+import pandas as pd
+import warnings
+from tkinter import *
+from tkinter import messagebox
 
-if version2 < 9:
-    print("Es menor")
+folder = os.path.abspath(os.getcwd())
+extensions = ('.pdf','.PDF','.docx','.DOCX','.png','.jpg')
 
-if version2 > 6:
-    print("Es Mayor")
+from pathlib import Path
+home_dir = Path.home()
+fileDB = (f'{ home_dir }\\Textile Sourcing Company S.A.C\\Soporte TI - General\\BD\\DB_TI.xlsx')
+filemovile = (f'{ home_dir }\\Textile Sourcing Company S.A.C\\Soporte TI - General\\Inventario_Equipos\\Equipos_Moviles\\Inventario_Equipos_Movil.xlsx')
+filelaptop = (f'{ home_dir }\\Textile Sourcing Company S.A.C\\Soporte TI - General\\Inventario_Equipos\\Equipos_de_Computo\\Inventario_Equipos_Computo.xlsx')
+
+for x in os.listdir(folder):
+    if x.endswith(extensions):
+        
+        filepath = os.path.splitext(os.path.basename(x))[0]
+        print(filepath)
+
+        if len(filepath) < 13:
+            warnings.simplefilter(action='ignore', category=UserWarning)
+            data = pd.read_excel(filelaptop,skiprows=1, sheet_name="Inventario_General")
+            df = pd.DataFrame(data)
+            laptopnumber = str(filepath)
+            columnas = ['Host', 'DNI', 'Colaborador', 'Área', 'Sede', 'SN']
+            
+            def buscar():
+                info = df[df['Host']==laptopnumber]
+                return(info)
+            df_seleccionados = buscar()[columnas]
+
+            for index, row in df_seleccionados.iterrows():
+                os.rename(folder + '//' + filepath + '.pdf', folder + '//' + str(row['Host'])
+                + ' - ' + str(int(row['DNI']))
+                + ' - ' + str.upper((row['Colaborador']))
+                + ' - ' + str.upper((row['Área']))
+                + ' - ' + str.upper((row['Sede']))
+                + ' - ' + str.upper((row['SN'])) +'.pdf')
+        else:
+            continue
